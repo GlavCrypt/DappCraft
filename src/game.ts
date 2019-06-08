0e12e46d720eaae500c9ca0b033010fc4e3dea1b
DCLNFTW <demyangolyshev@gmail.com>
2019-06-08 20:31:30 +0300

    added test payment btn

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,25 @@
 import { SlideDoorState, SliderDoorSystem } from "./modules/slide-door";
+import * as EthereumController from "@decentraland/EthereumController"
+
+const eth = EthereumController
+
+executeTask(async () => {
+  try {
+    const address = await eth.getUserAccount()
+    log(address)
+    myText.value = address
+  } catch (error) {
+    log(error.toString())
+  }
+})
+
+const myEntity = new Entity()
+const myText = new TextShape("Loading")
+myEntity.addComponent(myText)
+myEntity.addComponent(new Transform({
+  position: new Vector3(-1, 5, -1),
+}))
+engine.addEntity(myEntity)
 
 /*
 const camera = Camera.instance
@@ -50,10 +71,6 @@ engine.addEntity(glasses)
 const doorPivot = new Entity()
 doorPivot.addComponent(new Transform())
 
-//let collideBox = new BoxShape()
-//collideBox.withCollisions = true
-//doorPivot.addComponent(collideBox);
-
 const doorLeft = new Entity()
 doorLeft.addComponent(new Transform(
 {
@@ -83,8 +100,13 @@ doorRight.addComponent(new Transform(
   scale: new Vector3(2, 4, 0.2)
 }))
 
+const emitMaterial = new Material();
+emitMaterial.emissiveColor = Color3.Blue()
+emitMaterial.emissiveIntensity = 5;
+
 doorRight.setParent(doorPivot)
 doorRight.addComponent(new BoxShape())
+doorRight.addComponent(emitMaterial)
 doorRight.addComponent(new SlideDoorState(
   new Vector3(1, 2, 0),
 new Vector3(2.5, 2, 0)
@@ -110,4 +132,32 @@ function openDoor(parent: IEntity){
   }   
 }
 
-debugger
+
+
+const RedMaterial = new Material()
+RedMaterial.albedoColor = Color3.Red()
+
+const btn = new Entity()
+btn.addComponent(new BoxShape())
+btn.addComponent(RedMaterial)
+btn.addComponent(new Transform(
+  {
+    position: new Vector3(-1, 2, -1),
+    rotation: Quaternion.Euler(0, 0, 0),
+    scale: new Vector3(0.5, 0.3, 0.1)
+  }))
+btn.addComponent(
+  new OnClick(e => {
+    RedMaterial.albedoColor = Color3.White()
+    executeTask(async () => {
+      try {
+        await eth.requirePayment('0x2b70046B540fc32d5AF3fd6B6B8c1096f7328700', 0.001, "ETH")
+        log("Succsessfull")
+        RedMaterial.albedoColor = Color3.Red()
+      } catch (error) {
+        log("Failed")
+      }
+    })
+  })
+)
+engine.addEntity(btn)
